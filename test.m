ba6e6fecfd3aa981a3d929400e2e8d80b9ee030a
primetang <tanggefu@gmail.com>
2014-10-16 15:29:51 +0800

    update

--- test.m
+++ test.m
@@ -1,9 +1,5 @@
 points = [0, 15, 30, 26, 26, 0, 20, 10; 0, 3, 10, 20, 12, 0, 12, 4];
-figure(1);
-for i = 1:size(points,2)
-    hold on;
-    plot(points(1, i), points(2, i), '*r', 'LineWidth', 4);
-end
+plot(points(1, :), points(2, :), '*r', 'LineWidth', 4);
 polygon = minimal_convex_polygon(points);
 hold on;
 plot(polygon(1, :), polygon(2, :), 'LineWidth', 2);
